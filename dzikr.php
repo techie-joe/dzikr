3a7bbaf18171068b989d82e2257089a6505a1af7
Techie Joe <185759893+techie-joe@users.noreply.github.com>
2024-11-11 09:47:02 +0800

    fresh start

---
+++ dzikr.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @package Dzikr
+ * @version 1.0
+ */
+/*
+Plugin Name: Dzikr
+Plugin URI: https://techie-joe.github.io/page-plug/
+Description: This plugin symbolizes hope and enthusiasm of an entire human kind.
+Author: Techie Joe
+Version: 0.0.1
+Author URI: https://techie-joe.github.io/
+*/
+
+function get_kalima() {
+$kalima = "
+سُبْحَانَ اللهِ
+الْحَمْدُ للهِ
+لاَ إِلَهَ إِلاَّ الله
+اللهُ أَكْبَرُ
+حَسْبِيْ رَبِّيْ جَلَّ اللهُ
+مَا فِيْ قَلْبِيْ غَيْرُ اللهِ
+نُوْرُ مُحَمَّدٍ صَلَّى اللهُ
+تُبْنَا إِلَى اللهِ
+وَرَجَعْنَا إِلَى اللهِ
+وَنَدِمْنَا عَلَى مَا فَعَلْنَا
+سُبْحَانَ مَنْ لاَ يَقْدِرُ الْخَلْقُ قَدْرَهُ
+مَنْ هُوَ فَوْقَ الْعَرْشِ فَرْدٌ مُوَحَّدٌ
+مَلِيْكٌ عَلَى عَرْشِ السَّمَاءِ مُهَيْمِنُ
+لِعِزَّتِهِ تَعْنُو الْوُجُوْهُ وَتَسْجُدُ
+لَيْسَ لَهَا مِنْ دُوْنِ اللهِ كَاشِفَةٌ
+أَسْتَغْفِرُ اللهَ
+إِنَّ اللهَ كَانَ تَوَّابًا
+للهُمَّ صَلِّ عَلَى سَيِّدِنَا مُحَمَّدٍ
+";
+
+	// split it into lines.
+	$kalima = explode( "\n", $kalima );
+
+	// randomly choose a line.
+	return wptexturize( $kalima[ mt_rand( 0, count( $kalima ) - 1 ) ] );
+}
+
+// echoes the chosen line
+function dzikr() {
+	$chosen = get_kalima();
+	$lang   = '';
+	if ( 'ar_' !== substr( get_user_locale(), 0, 3 ) ) {
+		$lang = ' lang="ar"';
+	}
+
+	printf(
+		'<p id="dzikr"><span class="screen-reader-text">%s </span><span dir="rtl"%s>%s</span></p>',
+		__( 'Dzikr by Techie Joe', 'dzikr' ),
+		$lang,
+		$chosen
+	);
+}
+
+// Now we set that function up to execute when the admin_notices action is called.
+add_action( 'admin_notices', 'dzikr' );
+
+// We need some CSS to position the paragraph.
+function dzikr_css() {
+	echo "
+	<style type='text/css'>
+	#dzikr {
+		float: right;
+		padding: 0 10px;
+		margin: -3px 0;
+		font-size: 1.5rem;
+		line-height: 1.6666;
+		font-family: auto;
+	}
+	.rtl #dzikr {
+		float: left;
+	}
+	.block-editor-page #dzikr {
+		display: none;
+	}
+	@media screen and (max-width: 782px) {
+		#dzikr,
+		.rtl #dzikr {
+			float: none;
+			padding-left: 0;
+			padding-right: 0;
+		}
+	}
+	</style>
+	";
+}
+
+add_action( 'admin_head', 'dzikr_css' );
---
+++ readme.txt
@@ -0,0 +1,15 @@
+# === Dzikr ===
+Contributors: techie-joe
+Stable tag: 1.0.0
+Tested up to: 6.6.2
+Requires at least: 6.6.2
+
+This wordpress plugin symbolizes hope and enthusiasm of an entire human kind.
+
+# == Description ==
+
+This wordpress plugin symbolizes hope and enthusiasm of an entire human kind.
+
+# == License ==
+
+This is free and unencumbered software released into the public domain.
